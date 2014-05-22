337ba8c28c587f2c1dc758097015bfab90a8e592
EIva <astellar@gmail.com>
2014-05-22 23:53:39 +0400

    Several filters test. Seems Im doing something wrong: it all has ugly accuracy.

--- Simulation/IntTest.m
+++ Simulation/IntTest.m
@@ -7,16 +7,27 @@ T=(0:dt:TMax);
 deviation = @(t) normrnd(0, 1, size(t,1), size(t,2));
 ideal = @(t) 0;
 
+% % Функция Ускорения
+% tFuncA = @(t, div) [(log(T+exp(1))-1) + div(t);...
+%                     zeros(size(t,1), size(t,2)) + div(t);...
+%                     zeros(size(t,1), size(t,2)) + div(t)];
+%                 
+% % Функция угловой скорости
+% tFuncRot = @(t, div) [ sin(t*0.1) + div(t);...
+%                        sin(t*0.6) + div(t);...
+%                        sin(t*0.3) + div(t)];
+
 % Функция Ускорения
-tFuncA = @(t, div) [(log(T+exp(1))-1) + div(t);...
+tFuncA = @(t, div) [zeros(size(t,1), size(t,2)) + div(t);...
                     zeros(size(t,1), size(t,2)) + div(t);...
                     zeros(size(t,1), size(t,2)) + div(t)];
                 
 % Функция угловой скорости
-tFuncRot = @(t, div) [ sin(t*0.1) + div(t);...
-                       sin(t*0.6) + div(t);...
-                       sin(t*0.3) + div(t)];
+tFuncRot = @(t, div) [ zeros(size(t,1), size(t,2)) + div(t);...
+                       zeros(size(t,1), size(t,2)) + div(t);...
+                       zeros(size(t,1), size(t,2)) + div(t)];
 
+                   
 %% Подготовка к расчету позиции
 p0 = [0;0;0];
 r0 = p0;
@@ -27,18 +38,20 @@ Ideal = struct('Trj', zeros(3, length(T)),... Траектория
                'V', v0,... Текущая скорость Body
                'Acc', tFuncA(T,ideal),... Ускорения Body
                'RotV', tFuncRot(T,ideal)); % Угловые скорости Body
+           
 Estimated = struct('Trj', zeros(3, length(T)),...
                    'Pos', p0,...
                    'Rot', r0,...
                    'V', v0,...
                    'Acc', tFuncA(T,deviation),...
                    'RotV', tFuncRot(T,deviation));
-Kalman = struct('Trj', zeros(3, length(T)),...
-                   'Pos', p0,...
-                   'Rot', r0,...
-                   'V', v0,...
-                   'Acc', tFuncA(T,deviation),...
-                   'RotV', tFuncRot(T,deviation));
+               
+ABFilter = struct('Trj', zeros(3, length(T)),...
+                'Pos', p0,...
+                'Rot', r0,...
+                'V', v0,...
+                'Acc', tFuncA(T,deviation),...
+                'RotV', tFuncRot(T,deviation));
 %% Расчет Идеальной позиции
 
 for i=1:length(T)
@@ -69,12 +82,30 @@ for i=1:length(T)
     Estimated.Trj(:,i) = Estimated.Pos;
 end
 
-%% Рассчет предполагаемой позиции с применением фильтра Калмана
-
+%% Рассчет Предполагаемой позиции с упрощенным фильтром Калмана
+ABFilter.Aopt = [0;0;0];
+ABFilter.Ropt = [0;0;0];
+ABFilter.Kstab = 0.8; % параметр стабилизации.
+for i=1:length(T)
+    t = T(i);
+    ABFilter.dRot = ABFilter.RotV(:,i);
+    ABFilter.dAcc = ABFilter.Acc(:,i);
+    ABFilter.Ropt = ABFilter.dRot*ABFilter.Kstab + ABFilter.Ropt*(1-ABFilter.Kstab);
+    ABFilter.Aopt = ABFilter.dAcc*ABFilter.Kstab + ABFilter.Aopt*(1-ABFilter.Kstab);
+    ABFilter.Rot = ABFilter.Rot + ABFilter.Ropt * dt;
+    ABFilter.V = ABFilter.V + dt*ABFilter.Aopt;
+    Tr=rotz(ABFilter.Rot(3)) * rotx(ABFilter.Rot(1)) * roty(ABFilter.Rot(2));
+    ABFilter.Vf = Tr*ABFilter.V;
+    ABFilter.Pos = ABFilter.Pos + dt* ABFilter.Vf;
+    p = ABFilter.Pos;
+    ABFilter.Trj(:,i) = ABFilter.Pos;
+end
 
 %% Отображение
 plotTrj = @(trj, p) plot3(trj(1,:)', trj(2,:)', trj(3,:)', p);
 figure
 hold on
-plotTrj(Ideal.Trj, 'r-');
-plotTrj(Estimated.Trj, 'b-');
+plotTrj(Ideal.Trj, 'r*');
+plotTrj(Estimated.Trj, 'g-');
+plotTrj(ABFilter.Trj, 'b-');
+display(norm(Ideal.Trj-Estimated.Trj))
