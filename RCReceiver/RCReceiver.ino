6346f66802ae526227162e30a1a14626af31f500
EIva <astellar@gmail.com>
2014-12-29 23:51:47 +0300

    nRF->PPM encoder.

--- RCReceiver/RCReceiver.ino
+++ RCReceiver/RCReceiver.ino
@@ -16,7 +16,7 @@
  * 'ping_server_interupt' on the server.
  */
 
-#include <Servo.h> 
+
 #include <SPI.h>
 #include <Mirf.h>
 #include <nRF24L01.h>
@@ -38,6 +38,25 @@ struct Packet
 
 #pragma pack(pop)
 
+
+//////////////////////CONFIGURATION///////////////////////////////
+#define chanel_number 8  //set the number of chanels
+
+#define PPM_FrLen 22500  //set the PPM frame length in microseconds (1ms = 1000µs)
+#define PPM_PulseLen 300  //set the pulse length
+#define onState 1  //set polarity of the pulses: 1 is positive, 0 is negative
+#define sigPin 5  //set PPM signal output pin on the arduino
+
+#define default_servo_value 1500  //set the default servo value
+#define  SigMin  1000 // Min value for signal
+#define SigMax  2000 // Max value for signal
+//////////////////////////////////////////////////////////////////
+
+ 
+/*this array holds the servo values for the ppm signal
+ change theese values in your code (usually servo values move between 1000 and 2000)*/
+int ppm[chanel_number];
+
 union PacketSerializer
 {
   Packet data;
@@ -47,13 +66,11 @@ union PacketSerializer
 // Address of reciever.
 byte addr[]={0xDB,0xDB,0xDB,0xDB,0xDB};
 
-Servo _thr;
-Servo _yaw;
-Servo _pitch;
-Servo _roll;
+PacketSerializer packet;
+
 
 void setup(){
-  Serial.begin(9600);
+
   /*
    * Setup pins / SPI.
    */
@@ -98,7 +115,7 @@ void setup(){
    */
   Mirf.channel = 10; 
   Mirf.config();
-  
+  /*
   _thr.attach(3);
   _yaw.attach(5);
   _pitch.attach(6);
@@ -110,35 +127,99 @@ void setup(){
   _roll.write(0);
   
   Serial.println("Beginning ... "); 
+  */
+  
+    //initiallize default ppm values
+  for(int i=0; i<chanel_number; i++){
+    ppm[i]= default_servo_value;
+  }
+  ppm[0] = SigMin; // Throttle = 0;
+
+  pinMode(sigPin, OUTPUT);
+  digitalWrite(sigPin, !onState);  //set the PPM signal pin to the default state (off)
+  
+  cli();
+  TCCR1A = 0; // set entire TCCR1 register to 0
+  TCCR1B = 0;
+  
+  OCR1A = 100;  // compare match register, change this
+  TCCR1B |= (1 << WGM12);  // turn on CTC mode
+  TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  sei();
 }
 
-PacketSerializer packet;
+const int THR = 2;
+const int YAW = 3;
+const int PTC = 1;
+const int ROL = 0;
+const int BT1 = 4;
+const int BT2 = 5;
+const int BT3 = 6;
 
 void loop(){
   unsigned long time = millis();
-  const int _min = 40;
-  const int _max = 140;
-  
 
   delay(10);
+
   while(!Mirf.dataReady()){
     //Serial.println("Waiting");
     if ( ( millis() - time ) > 1000 ) {
-      Serial.println("Timeout on response from server!");
-      _thr.write(_min);
+      //Serial.println("Timeout on response from server!");
+      //_thr.write(_min);
+      ppm[THR] = 0; // How to enable failover?
       return;
     }
   }
   
   Mirf.getData((byte *)packet.serialized);
   
-
+  ppm[THR] = map(packet.data.THR,0,1024,SigMin,SigMax);
+  ppm[YAW] = map(packet.data.YAW,0,1024,SigMin,SigMax+300);  
+  ppm[PTC] = map(packet.data.PTC,0,1024,SigMin,SigMax);
+  ppm[ROL] = map(packet.data.ROL,0,1024,SigMin,SigMax);
+  ppm[BT1] = packet.data.BT1?SigMax:SigMin;
+  ppm[BT2] = packet.data.BT2?SigMax:SigMin;
+  ppm[BT3] = packet.data.BT3?SigMax:SigMin;
+  
+/*
   _thr.write(  map(packet.data.THR,0,1024,_min,_max));
   _yaw.write(  map(packet.data.YAW,0,1024,_min,_max));  
   _pitch.write(map(packet.data.PTC,0,1024,_min,_max));
   _roll.write( map(packet.data.ROL,0,1024,_min,_max));
+  */
   //Serial.print(".");
 } 
   
+ISR(TIMER1_COMPA_vect){  //leave this alone
+  static boolean state = true;
   
+  TCNT1 = 0;
+  
+  if(state) {  //start pulse
+    digitalWrite(sigPin, onState);
+    OCR1A = PPM_PulseLen * 2;
+    state = false;
+  }
+  else{  //end pulse and calculate when to start the next pulse
+    static byte cur_chan_numb;
+    static unsigned int calc_rest;
+  
+    digitalWrite(sigPin, !onState);
+    state = true;
+
+    if(cur_chan_numb >= chanel_number){
+      cur_chan_numb = 0;
+      calc_rest = calc_rest + PPM_PulseLen;// 
+      OCR1A = (PPM_FrLen - calc_rest) * 2;
+      calc_rest = 0;
+    }
+    else{
+      OCR1A = (ppm[cur_chan_numb] - PPM_PulseLen) * 2;
+      calc_rest = calc_rest + ppm[cur_chan_numb];
+      cur_chan_numb++;
+    }     
+  }
+}
+
   
